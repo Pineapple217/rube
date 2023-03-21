90be4c8089ec11544bd70a0d1b17926d17da0391
Pineapple217 <rosierjules@gmail.com>
2023-03-21 12:50:01 +0100

    cos/sin caching

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,14 @@ use std::io::{self, Write};
 use term_size;
 
 const CUBE_INIT_STATE: [[f32; 3]; 8] = [
-    // Front face
-    [-1.0, -1.0, 1.0],
-    [1.0, -1.0, 1.0],
     [1.0, 1.0, 1.0],
+    [1.0, -1.0, 1.0],
+    [-1.0, -1.0, 1.0],
     [-1.0, 1.0, 1.0],
-    // Back face
+    [-1.0, 1.0, -1.0],
     [-1.0, -1.0, -1.0],
     [1.0, -1.0, -1.0],
     [1.0, 1.0, -1.0],
-    [-1.0, 1.0, -1.0],
 ];
 
 struct Cube {
@@ -24,16 +22,24 @@ struct FrameSize {
 }
 
 fn draai(x: &f32, y: &f32, z: &f32, a: &f32, b: &f32, c: &f32) -> (f32, f32, f32) {
-    let new_x = y * a.sin() * b.sin() * c.cos() - z * a.cos() * b.sin() * c.cos()
-        + y * a.cos() * c.sin()
-        + z * a.sin() * c.sin()
-        + x * b.cos() * c.cos();
+    let a_sin = a.sin();
+    let b_sin = b.sin();
+    let c_sin = c.sin();
 
-    let new_y = y * a.cos() * c.cos() + z * a.sin() * c.cos() - y * a.sin() * b.sin() * c.sin()
-        + z * a.cos() * b.sin() * c.sin()
-        - x * b.cos() * c.sin();
+    let a_cos = a.cos();
+    let b_cos = b.cos();
+    let c_cos = c.cos();
 
-    let new_z = z * a.cos() * b.cos() - y * a.sin() * b.cos() + x * b.sin();
+    let new_x = y * a_sin * b_sin * c_cos - z * a_cos * b_sin * c_cos
+        + y * a_cos * c_sin
+        + z * a_sin * c_sin
+        + x * b_cos * c_cos;
+
+    let new_y = y * a_cos * c_cos + z * a_sin * c_cos - y * a_sin * b_sin * c_sin
+        + z * a_cos * b_sin * c_sin
+        - x * b_cos * c_sin;
+
+    let new_z = z * a_cos * b_cos - y * a_sin * b_cos + x * b_sin;
 
     (new_x, new_y, new_z)
 }
@@ -48,12 +54,12 @@ fn render_cube(cube: &Cube, frame_size: &FrameSize) -> Vec<Vec<char>> {
     let mut prev_corner: Option<(usize, usize)> = None;
     for corner in &cube.vertices {
         let pos_x = ((corner[0] * size as f32 * 0.4) + frame_size.x as f32 * 0.5) as usize;
-        let pos_y = ((corner[1] * size as f32 * 0.4 * 0.6) + frame_size.y as f32 * 0.5) as usize;
+        let pos_y = ((corner[1] * size as f32 * 0.4 * 0.55) + frame_size.y as f32 * 0.5) as usize;
         // frame[pos_y][pos_x] = '#';
         if let Some((p_x, p_y)) = prev_corner {
             for (x, y) in bresenham_line(*&pos_x as i32, *&pos_y as i32, *&p_x as i32, *&p_y as i32)
             {
-                frame[y][x] = '#'
+                frame[y][x] = '*'
             }
         }
 
@@ -102,26 +108,6 @@ fn bresenham_line(x0: i32, y0: i32, x1: i32, y1: i32) -> Vec<(usize, usize)> {
         (y1 as usize).try_into().unwrap(),
     ));
     pixels
-    // let mut pixels = vec![];
-    // let dx = x1 as f32 - x0 as f32;
-    // let dy = y1 as f32 - y0 as f32;
-    // let slope = dy.abs() / dx.abs();
-    // let (mut x, mut y) = (x0 as f32, y0 as f32);
-    // let (xstep, ystep) = if dx > 0.0 {
-    //     (1, slope as i32)
-    // } else {
-    //     (-1, -(slope as i32))
-    // };
-
-    // pixels.push((x as usize, y as usize));
-
-    // for _ in 0..dx.abs() as i32 {
-    //     x += xstep as f32;
-    //     y += ystep as f32;
-    //     pixels.push((x as usize, y as usize));
-    // }
-
-    // pixels
 }
 
 fn main() {
